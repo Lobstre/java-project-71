2f98db1c5b3ab70b2e3eda948acf7e67715e900d
ilya yaranov <di-we@outlook.com>
2022-11-19 00:21:25 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+    @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "Hello world 1.0",
+        description = "Compares two configuration files and shows a difference.")
+    class HelloWorld implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("Hello world!");
+        return "Hello world!";
+    }
+}
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new HelloWorld()).execute(args);
+        System.exit(exitCode);
     }
 }
